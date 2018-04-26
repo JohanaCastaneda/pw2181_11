a82d1bd6b516504cd1ff68ca1eeb6c6f58a3950c
JORGE MICHEL LOPEZ ECHAGARAY <norkasrom@gmail.com>
2018-04-26 12:03:21 -0600

    Comics y Series

--- Marvel/js/index.js
+++ Marvel/js/index.js
@@ -8,6 +8,9 @@ var marvel=function()
 		var cantidadComics = 0;
 		var comics = "";
 
+		var cantidadSeries = 0;
+		var series = "";
+
 		url=url + personaje;
 
 		$.ajax({
@@ -18,12 +21,52 @@ var marvel=function()
 				if(response.code == 200)//OK
 				{
 					//console.log(response);
+
+					$("#nombre").attr("src",response.data.results[0].name);
 					$("#foto").attr("src",response.data.results[0].thumbnail.path+"."+response.data.results[0].thumbnail.extension);
+					//alert(response.data.results[0].name)
+					cantidadComics=response.data.results[0].comics.returned;
+
+					cantidadSeries=response.data.results[0].series.returned;
+
+					//COMICS
+
+					for (var i = 0; i < cantidadComics; i++) 
+					{
+						comics+=response.data.results[0].comics.items[i].name+"<br>";
+					}
+					$("#comics").html(comics);
+
+					//SERIES
+
+					for (var i = 0; i < cantidadSeries; i++) 
+					{
+						series+=response.data.results[0].series.items[i].name+"<br>";
+					}
+					$("#series").html(series);
+
+
 				}
 			}
+
+
 		});
 	}
+	var teclapersonaje = function(tecla)
+	{
+		//13 + 10
+		//retorno de carro y avance de linea
+
+		if(tecla.which == 13)//Enter
+		{
+			Buscar();
+		}
+	}
 	$("#btnBuscar").on("click",Buscar);
+	$("#txtPersonaje").on("keypress",teclapersonaje) //validar tecla
 
 }
 $(document).ready(marvel);
+
+
+
