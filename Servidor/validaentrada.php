06f461028090aeee16fb66a1433c0da414021966
CCDM17 <ccdm17@CCDM17.local>
2018-05-23 12:07:19 -0600

    ocultar el nombre del usuario y la clave

---
+++ Servidor/validaentrada.php
@@ -0,0 +1,33 @@
+<?php
+include 'conexiones.php';
+function valida()
+{
+	$respuesta = false;
+	$usuario = $_POST["usuario"];
+	$clave = md5($_POST["clave"]);
+	//conectarnos al servidor de BD
+	$con=conecta();
+	$consulta="select * from usuario where usuario= '".$usuario."' and clave='".$clave. "' limit 1"; ///hace que la consulta se ejecute mas rapido
+
+	//echo $consulta;
+
+	$resConsulta= mysqli_query($con,$consulta);
+
+	if(mysqli_num_rows($resConsulta) > 0){
+		$respuesta = true;
+	}
+
+	$salidaJSON = array ('respuesta' => $respuesta);
+	print json_encode($salidaJSON);
+}
+
+$opc = $_POST["opc"];
+switch ($opc) 
+{
+	case 'validaentrada':
+	valida();
+	break;
+	
+	default:
+	break;
+}
